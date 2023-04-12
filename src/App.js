64127498ab99872ccac41ab4bbb32730322c6895
Matthew Egan <mnegan93@gmail.com>
2023-04-13 02:36:27 +1200

    Add dark mode with persistence

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
       <header className="App-header">
         <h1>
           Github Profile Viewer
         </h1>
+        <button 
+          onClick={() => 
+            theme === 'light' ? setTheme('dark') : setTheme('light')
+          }
+          className="theme-button"
+        >
+          ☀/☾
+        </button>
       </header>
         <SearchBar />
     </div>
